c6a6a9241131954bbe4e98c4668bafd9415dbf12
KoysX <2685697179b@gmail.com>
2023-07-05 09:46:30 +0800

    Sync with latest TrebleDroid source

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -6,17 +6,17 @@ source="$(readlink -f -- $1)"
 trebledroid="$source/patches/trebledroid"
 personal="$source/patches/pre"
 
-printf "\n ### APPLYING TREBLEDROID PATCHES ###\n";
+printf "\n### APPLYING PERSONAL PATCHES ###\n";
 sleep 1.0;
-for path in $(cd $trebledroid; echo *); do
-	tree="$(tr _ / <<<$path | sed -e 's;platform/;;g')"
-	printf "\n| $path ###\n";
+for path_personal in $(cd $personal; echo *); do
+	tree="$(tr _ / <<<$path_personal | sed -e 's;platform/;;g')"
+	printf "\n| $path_personal ###\n";
 	[ "$tree" == build ] && tree=build/make
     [ "$tree" == vendor/hardware/overlay ] && tree=vendor/hardware_overlay
     [ "$tree" == treble/app ] && tree=treble_app
 	pushd $tree
 
-	for patch in $trebledroid/$path/*.patch; do
+	for patch in $personal/$path_personal/*.patch; do
 		# Check if patch is already applied
 		if patch -f -p1 --dry-run -R < $patch > /dev/null; then
             printf "### ALREDY APPLIED: $patch \n";
@@ -39,17 +39,17 @@ for path in $(cd $trebledroid; echo *); do
 	popd
 done
 
-printf "\n### APPLYING PERSONAL PATCHES ###\n";
+printf "\n ### APPLYING TREBLEDROID PATCHES ###\n";
 sleep 1.0;
-for path_personal in $(cd $personal; echo *); do
-	tree="$(tr _ / <<<$path_personal | sed -e 's;platform/;;g')"
-	printf "\n| $path_personal ###\n";
+for path in $(cd $trebledroid; echo *); do
+	tree="$(tr _ / <<<$path | sed -e 's;platform/;;g')"
+	printf "\n| $path ###\n";
 	[ "$tree" == build ] && tree=build/make
     [ "$tree" == vendor/hardware/overlay ] && tree=vendor/hardware_overlay
     [ "$tree" == treble/app ] && tree=treble_app
 	pushd $tree
 
-	for patch in $personal/$path_personal/*.patch; do
+	for patch in $trebledroid/$path/*.patch; do
 		# Check if patch is already applied
 		if patch -f -p1 --dry-run -R < $patch > /dev/null; then
             printf "### ALREDY APPLIED: $patch \n";
